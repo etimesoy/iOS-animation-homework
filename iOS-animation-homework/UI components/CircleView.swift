42f71cd59a87ef386ede7ddee7100b1129f949a8
Ruslan Gazizov <ruslangazizov36@gmail.com>
2022-04-09 13:57:46 +0300

    Add number label for circle view

--- iOS-animation-homework/UI components/CircleView.swift
+++ iOS-animation-homework/UI components/CircleView.swift
@@ -8,47 +8,46 @@
 import UIKit
 
 class CircleView: UIView {
-    var circleLayer: CAShapeLayer!
+    // MARK: UI components
+    
+    lazy var circleLayer: CAShapeLayer = {
+        let arcCenter = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+        let circlePath = UIBezierPath(arcCenter: arcCenter,
+                                      radius: arcCenter.x,
+                                      startAngle: -.pi / 2,
+                                      endAngle: 3 * .pi / 2,
+                                      clockwise: true)
+        
+        circleLayer = CAShapeLayer()
+        circleLayer.path = circlePath.cgPath
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.strokeColor = UIColor.green.cgColor
+        circleLayer.lineWidth = 10
+        circleLayer.strokeEnd = 0
+        return circleLayer
+    }()
+    
+    // MARK: Initializers
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        addCirclePath()
+        layer.addSublayer(circleLayer)
+        configureColorAnimation()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func addCirclePath() {
-        let arcCenter = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
-        let circlePath = UIBezierPath(arcCenter: arcCenter,
-                                      radius: arcCenter.x,
-                                      startAngle: -.pi / 2,
-                                      endAngle: .pi * 2,
-                                      clockwise: true)
-        
-        circleLayer = CAShapeLayer()
-        circleLayer.path = circlePath.cgPath
-        circleLayer.fillColor = UIColor.clear.cgColor
-        circleLayer.strokeColor = UIColor.red.cgColor
-        circleLayer.lineWidth = 5
-        
-        circleLayer.strokeEnd = 0
-        
-        layer.addSublayer(circleLayer)
-    }
+    // MARK: Animations
     
-    func animateCircle(duration: TimeInterval) {
-        let animation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
-
-        animation.duration = duration
-        animation.fromValue = 0
-        animation.toValue = 1
-
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-
-        circleLayer.strokeEnd = 1.0
-        circleLayer.add(animation, forKey: "animateCircle")
+    func configureColorAnimation() {
+        let animation = CABasicAnimation(keyPath: "strokeColor")
+        animation.duration = 5
+        animation.fromValue = UIColor.red.cgColor
+        animation.toValue = UIColor.green.cgColor
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        circleLayer.add(animation, forKey: "strokeColor")
     }
 }
--- iOS-animation-homework/UI components/ViewController.swift
+++ iOS-animation-homework/UI components/ViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: UI components
+    
+    private lazy var numberLabel: UILabel = {
+        let width: CGFloat = 100, height: CGFloat = 50
+        let x = view.center.x - width / 2, y = view.center.y - height / 2
+        let label = UILabel(frame: CGRect(x: x, y: y, width: width, height: height))
+        
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 30, weight: .semibold)
+        label.text = "0"
+        
+        return label
+    }()
+    
     private lazy var circleView: CircleView = {
         let width: CGFloat = 200
         let height = width
@@ -17,15 +31,50 @@ class ViewController: UIViewController {
         return CircleView(frame: CGRect(x: x, y: y, width: width, height: height))
     }()
     
+    // MARK: Properties
+    
+    private var animator: UIViewPropertyAnimator!
+    
+    // MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addSubviews()
+        view.addSubview(circleView)
+        view.addSubview(numberLabel)
+        configureAnimator()
+        startNumberLoading()
     }
     
-    private func addSubviews() {
-        view.addSubview(circleView)
-        
-        circleView.animateCircle(duration: 3.0)
+    // MARK: Animations
+    
+    private func configureAnimator() {
+        let newPoint = CGPoint(x: self.view.center.x, y: self.view.center.y / 2)
+        animator = UIViewPropertyAnimator(duration: 0.5, curve: .linear, animations: {
+            self.circleView.center = newPoint
+            self.numberLabel.center = newPoint
+        })
+    }
+    
+    private func animateCircleViewScaling() {
+        UIView.animate(withDuration: 1) {
+            self.circleView.transform = .init(scaleX: 10, y: 10)
+        }
+    }
+    
+    private func startNumberLoading() {
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { [weak self] timer in
+            let value = Int(self?.numberLabel.text ?? "0") ?? 0
+            let nextValue = value + 1
+            
+            self?.numberLabel.text = String(nextValue)
+            self?.circleView.circleLayer.strokeEnd = CGFloat(nextValue) / 100
+            self?.animator.fractionComplete = CGFloat(nextValue) / 100
+            
+            if nextValue == 100 {
+                timer.invalidate()
+                self?.animateCircleViewScaling()
+            }
+        }
     }
 }
